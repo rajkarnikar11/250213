8efb912c6060b6c2b33072141fa9211026818e36
salil <salil.rajkarnikar@yipl.com.np>
2025-01-29 22:07:54 +0545

    wip

--- src/components/ImageViewerWithPinned.js
+++ src/components/ImageViewerWithPinned.js
@@ -11,7 +11,7 @@ const ImageViewerWithPinned = ({
     <div
       className={`flex relative items-center  my-auto justify-center ${className} relative  `}
     >
-      <div className="relative pl-10 page-bg flex-col items-center flex  h-[468px] w-[calc(100%-16px)] p-4 m-4 ml-0 mx-auto border-2 border-gray-900 ">
+      <div className="relative pl-10 page-bg flex-col items-center flex  h-[468px] w-[calc(100%-16px)] py-3 px-4 m-4 ml-0 mx-auto border-2 border-gray-900 ">
         {header ? (
           <h6 className="book-header px-8 py-1 mb-4 font-medium mt-2 text-[40px] max-w-[90%] text-gray-900 bg-opacity-90 border-2 border-[#433e36]  story ">
             {header}
@@ -33,7 +33,7 @@ const ImageViewerWithPinned = ({
             alt={text}
           />
         </div>
-        <p className=" p-2 mt-4 min-w-[200px] text-center text-2xl paper-effect max-w-[90%] text-gray-900 bg-opacity-90 border-2 border-[#433e36]  backdrop-blur-sm  story ">
+        <p className=" !p-2 mt-4 min-w-[200px] text-center text-2xl paper-effect max-w-[90%] text-gray-900 bg-opacity-90 border-2 border-[#433e36]  backdrop-blur-sm  story ">
           {text}
         </p>
       </div>
